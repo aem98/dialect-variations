e00aab7b2487fe9e0b518f5bc077db556e5b7c36
sfk14 <sfk14@pitt.edu>
2016-04-17 02:29:08 -0400

    working solution applied to preps

--- js/amman.js
+++ js/amman.js
@@ -4,32 +4,27 @@ function init(){
     var clickables = document.getElementsByClassName("clickable");
     for (var i = 0; i < clickables.length; i++){
         clickables[i].addEventListener('click', sendRequest, false)
-        console.log("1");
     }
 }
 
 function sendRequest(){
     id = this.className.split(' ')[1];
     excerptNum = this.innerHTML;
-    console.log("2");
+    
     // request to server
     var req;
     if(window.XMLHttpRequest){
-        console.log("not IE");
         req = new XMLHttpRequest();
     } else{
     // for IE5, IE6
         req = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    console.log("3");
     // p to display excerpt
     var selectedP = document.getElementById(id);
-    console.log("4");
+    
     req.onreadystatechange = function(){
-        console.log("5");
         // request finished/response ready, status ok
         if(req.readyState == 4 && req.status == 200){
-            console.log("7");
             xmlSource = req.responseXML;
             // gets excerpts from XML
             var excerpts = xmlSource.getElementsByTagName("excerpt");
