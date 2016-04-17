44f8b0f3ab135b3d997fb3fb95a443b875c0f9e0
sfk14 <sfk14@pitt.edu>
2016-04-17 01:14:00 -0400

    console logs

--- js/amman.js
+++ js/amman.js
@@ -4,10 +4,12 @@ function init(){
     var clickables = document.getElementsbyClassName("clickable");
     for (var i = 0; i < clickables.length; i++){
         clickables[i].addEventListener("click", sendRequest(this.className.split(' ')[1], this.innerHTML), false)
+        console.log("1");
     }
 }
 
 function sendRequest(id, excerptNum){
+    console.log("2");
     // request to server
     var req;
     if(window.XMLHttpRequest){
@@ -16,18 +18,22 @@ function sendRequest(id, excerptNum){
     // for IE5, IE6
         req = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    
+    console.log("3");
     req.onreadystatechangte = getExcerpt(id, excerptNum);
+    console.log("4");
     req.open("GET", "../data/project.xml", true);
     req.send();
+    console.log("5");
 }
 
 function getExcerpt(id, excerptNum){
+    console.log("6");
     // p to display excerpt
     var selectedP = document.getElementById(id);
 
     // request finished/response ready, status ok
     if(req.readyState == 4 && req.status == 200){
+        console.log("7");
         xmlSource = req.responseXML;
         // gets excerpts from XML
         var excerpts = xmlSource.getElementsByTagName("excerpt");
