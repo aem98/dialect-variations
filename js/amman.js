9959514c5ade48b9ac755b35ff57fdbbdab1ef1f
sfk14 <sfk14@pitt.edu>
2016-04-18 13:12:17 -0400

    textContent

--- js/amman.js
+++ js/amman.js
@@ -29,7 +29,7 @@ function sendRequest(){
             // gets excerpts from XML
             var excerpts = xmlSource.getElementsByTagName("excerpt");
             // display excerpt in p
-            selectedP.innerHTML = excerpts[excerptNum - 1].innerHTML;
+            selectedP.innerHTML = excerpts[excerptNum - 1].textContent;
         }
     };
     req.open("GET", "../data/project.xml", true);
